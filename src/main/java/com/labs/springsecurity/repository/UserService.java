8b131957b649d820eddcdde64102e3f0d667ad90
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-06-02 01:10:58 -0300

    Implementação do método loadUserByUsername(String login) retornando um UserDetail com a busca por login na classe UserService

--- src/main/java/com/labs/springsecurity/repository/UserService.java
+++ src/main/java/com/labs/springsecurity/repository/UserService.java
@@ -13,8 +13,8 @@ public class UserService implements UserDetailsService {
 	private UserRepository userRepository;
 	
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		return userRepository.findByLogin(login);
 	}
 
 }

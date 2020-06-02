8c7ff2002a592008ab881653f1ec1a4837d6183e
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-06-02 01:07:12 -0300

    User entity implements UserDetails; User entity com novas modificações: booleans setted to true; método getUsername retorna login; método getAuthorities retorna uma coleção de usuários administrativos

--- src/main/java/com/labs/springsecurity/entity/User.java
+++ src/main/java/com/labs/springsecurity/entity/User.java
@@ -1,9 +1,16 @@
 package com.labs.springsecurity.entity;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -24,4 +31,25 @@ public class User {
 	
 	private String login;
     private String password;
+    
+    @Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority("ADMIN"));
+	}
+	
+	@Override
+	public String getUsername() { return login; }
+	
+//	Booleans setted to true
+	@Override
+	public boolean isAccountNonExpired() { return true; }
+	
+	@Override
+	public boolean isAccountNonLocked() { return true; }
+	
+	@Override
+	public boolean isCredentialsNonExpired() { return true; }
+	
+	@Override
+	public boolean isEnabled() { return true; }
 }

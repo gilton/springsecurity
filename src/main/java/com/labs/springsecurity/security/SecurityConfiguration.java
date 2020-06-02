200c0c9ae94b2ac4035d1bf7c8b079179a8c32a1
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-06-02 02:16:02 -0300

    Implementação dos métodos: authenticationManagerBean(), configure(WebSecurity web), configure(AuthenticationManagerBuilder auth) e passwordEnconder() na classe SecurityConfiguration

--- src/main/java/com/labs/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/com/labs/springsecurity/security/SecurityConfiguration.java
@@ -1,7 +1,14 @@
 package com.labs.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -9,5 +16,29 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer
 @EnableResourceServer
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return authenticationManagerBean();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	public PasswordEncoder passwordEncoder() { return NoOpPasswordEncoder.getInstance(); }
 }

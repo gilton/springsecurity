71d0c0a96e17ebf479a0d737ce325553ab05b891
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-06-02 01:22:53 -0300

    Remoção da classe UserService do pacote repository para o pacote service

--- src/main/java/com/labs/springsecurity/service/UserService.java
+++ src/main/java/com/labs/springsecurity/service/UserService.java
@@ -1,10 +1,12 @@
-package com.labs.springsecurity.repository;
+package com.labs.springsecurity.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.labs.springsecurity.repository.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService {
